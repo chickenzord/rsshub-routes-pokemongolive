31f3a8cc5b80c0476045c7fad7a8d2a2c1b0fa6d
Akhyar Amarullah <akhy@chickenzord.com>
2024-09-27 10:37:56 +0700

    feat: news route

---
+++ namespace.ts
@@ -0,0 +1,8 @@
+import type { Namespace } from '@/types';
+
+export const namespace: Namespace = {
+    name: 'Pokémon Go Live',
+    url: 'pokemongolive.com',
+    categories: ['game'],
+    description: 'Pokémon Go is a 2016 augmented reality mobile game, part of the Pokémon franchise, developed and published by Niantic in collaboration with Nintendo and The Pokémon Company for iOS and Android devices.',
+};
---
+++ news.ts
@@ -0,0 +1,77 @@
+import { Data, DataItem, Route, ViewType } from '@/types';
+import ofetch from '@/utils/ofetch';
+import { parseDate } from '@/utils/parse-date';
+import { load } from 'cheerio';
+
+const feedTitle = 'Pokemon Go News';
+const baseURL = 'https://pokemongolive.com';
+const dateFormat = {
+    en: 'MMMM D, YYYY',
+    id: 'D MMMM YYYY',
+};
+
+export const route: Route = {
+    path: '/news/:lang?',
+    categories: ['game'],
+    view: ViewType.Pictures,
+    example: '/pokemongolive/news/en',
+    parameters: {},
+    features: {
+        requireConfig: false,
+        requirePuppeteer: false,
+        antiCrawler: false,
+        supportBT: false,
+        supportPodcast: false,
+        supportScihub: false,
+    },
+    radar: [
+        {
+            source: ['pokemongolive.com/news'],
+            target: '/news',
+        },
+    ],
+    name: feedTitle,
+    maintainers: ['akhy'],
+    url: 'pokemongolive.com/news',
+    handler: async (ctx) => {
+        const { lang = 'en' } = ctx.req.param();
+        const fullURL = `${baseURL}/news?hl=${lang}`;
+        const response = await ofetch(fullURL);
+
+        const $ = load(response);
+
+        const imageURL = $('.navbar__logo img').first().attr('src');
+
+        const list = $('a.blogList__post')
+            .toArray()
+            .map((i) => {
+                const item = $(i);
+                const itemTitle = item.find('.blogList__post__content__title').first();
+                const itemPubDate = item.find('.blogPost__publishDate').first();
+                const itemImage = item.find('.blogList__post__image img.image').first();
+
+                const result: DataItem = {
+                    title: itemTitle.text(),
+                    link: item.attr('href') || '',
+                    description: `<img src="${itemImage.attr('src')}"/><br/>`,
+                    image: itemImage.attr('src'),
+                    pubDate: parseDate(itemPubDate.text(), dateFormat[lang] || dateFormat.en, lang),
+                    language: lang,
+                };
+
+                return result;
+            });
+
+        const data: Data = {
+            title: feedTitle,
+            link: fullURL,
+            item: list,
+            image: imageURL,
+            allowEmpty: false,
+            ttl: 60,
+            language: lang,
+        };
+
+        return data;
+    },
+};
